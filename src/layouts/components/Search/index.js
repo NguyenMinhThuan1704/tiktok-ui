27deaaee73e6d6dd4a05e0d7cbf25207bd809e66
NguyenMinhThuan1704 <thuanpro9b@gmail.com>
2024-03-21 21:22:58 +0700

    Tối ưu code header #1

--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -3,7 +3,8 @@ import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import HeadlessTippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
-import * as searchServices from '~/apiServices/searchServices';
+
+import * as searchServices from '~/services/searchService';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
@@ -60,6 +61,8 @@ function Search() {
     };
 
     return (
+        // Using a wrapper <div> tag around the reference element solves
+        // this by creating a new parentNode context.
         <div>
             <HeadlessTippy
                 interactive
