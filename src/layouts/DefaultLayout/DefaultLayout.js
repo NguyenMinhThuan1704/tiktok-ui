0421f75ea9a2e851204e796222ce4d6956ac511b
NguyenMinhThuan1704 <thuanpro9b@gmail.com>
2024-03-22 22:22:12 +0700

    SideBar #1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import Header from '~/layouts/components/Header/Header';
+import Header from '~/layouts/components/Header';
+import Sidebar from '~/layouts/components/Sidebar';
 import styles from './DefaultLayout.module.scss';
-import Sidebar from './Sidebar/Sidebar';
 
 const cx = classNames.bind(styles);
 
@@ -17,9 +17,7 @@ function DefaultLayout({ children }) {
         </div>
     );
 }
-
 DefaultLayout.propTypes = {
     children: PropTypes.node.isRequired,
 };
-
 export default DefaultLayout;
